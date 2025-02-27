7453067a2b2bda8dc5ec5db0542f1e14125627f9
prsntprsnt <prsntprsnt@abc.com>
2025-02-27 23:56:59 +0900

    commit

--- java/calc/src/App.java
+++ java/calc/src/App.java
@@ -1,5 +1,10 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //System.out.println("Hello, World!");
+
+        int suc = 1453452;
+        int fai = 701234;
+
+        System.out.println();
     }
 }
